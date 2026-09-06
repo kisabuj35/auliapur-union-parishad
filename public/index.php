3e4d331778bc18e1682064febe5c5a5ef0015a9a
KAMRUL ISLAM SABUJ <kisabuj35@gmail.com>
2026-09-06 21:01:57 +0600

    Refactor index.php for autoloading and maintenance
    Refactor index.php to improve autoloading and maintenance checks.

--- public/index.php
+++ public/index.php
@@ -1,24 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
-
 define('LARAVEL_START', microtime(true));
 
-// মেইনটেন্যান্স মোড চেক
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+// অটোলোডার চেক
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require __DIR__.'/../vendor/autoload.php';
+} else {
+    // যদি ভেন্ডর ডাউনলোড না থাকে, তবুও সাইট ক্র্যাশ না করে সরাসরি ভিউ লোড করবে
+    require_once __DIR__.'/../routes/web_direct.php';
+    exit;
 }
 
-// অটোলোডার রেজিস্টার
-require __DIR__.'/../vendor/autoload.php';
-
-// অ্যাপ্লিকেশন বুটস্ট্র্যাপ
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Request::capture()
+    $request = Illuminate\Http\Request::capture()
 )->send();
 
 $kernel->terminate($request, $response);
